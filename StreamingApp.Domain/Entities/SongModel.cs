95cb04423ffb11704717c145e9212ead78f910f6
Xotrab <mokrylisc@gmail.com>
2021-12-01 21:03:26 +0100

    Created the SongModel

---
+++ StreamingApp.Domain/Entities/SongModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace StreamingApp.Domain.Entities
+{
+    public class SongModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Url { get; set; }
+
+        [Required]
+        public string Genre { get; set; }
+
+        [Required]
+        public int Playbacks { get; set; }
+
+        [Required]
+        public DateTime AddedOn { get; set; }
+
+        [Required]
+        public ICollection<PlaylistModel> Playlists { get; set; }
+
+        [Required]
+        public int AuthorId { get; set; }
+
+        [Required]
+        public ApplicationUser Author { get; set; }
+
+        [Required]
+        public ICollection<ApplicationUser> LikedBy { get; set; }
+    }
+}
