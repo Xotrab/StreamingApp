f47be720fdc6efed12ab870918567dcef3f96d1f
Xotrab <mokrylisc@gmail.com>
2021-12-01 21:03:46 +0100

    Created the ApplicationUser model

---
+++ StreamingApp.Domain/Entities/ApplicationUser.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+
+namespace StreamingApp.Domain.Entities
+{
+    public class ApplicationUser : IdentityUser<int>
+    {
+        public ICollection<SongModel> Songs { get; set; }
+
+        public ICollection<PlaylistModel> Playlists { get; set; }
+
+        public ICollection<SongModel> LikedSongs { get; set; }
+
+        public ICollection<PlaylistModel> LikedPlaylists { get; set; }
+
+        public ICollection<ApplicationUser> Followed { get; set; }
+    }
+}
