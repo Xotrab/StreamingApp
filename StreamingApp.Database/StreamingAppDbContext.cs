1344f79f8c0407d31467b8b3795949fba11ea5b2
Xotrab <mokrylisc@gmail.com>
2021-12-01 21:07:23 +0100

    Added the db context class, which inherits from the Identity context and has two DbSet properties - one for the Songs and the other one for the Playlists

---
+++ StreamingApp.Database/StreamingAppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using StreamingApp.Domain.Entities;
+
+namespace StreamingApp.Database
+{
+    public class StreamingAppDbContext : IdentityDbContext
+    {
+        public StreamingAppDbContext(DbContextOptions options): base(options)
+        {
+
+        }
+
+        public DbSet<SongModel> Songs { get; set; }
+
+        public DbSet<PlaylistModel> Playlists { get; set; }
+    }
+}
