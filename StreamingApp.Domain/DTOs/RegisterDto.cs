34ab600ac3f6e57913605c461fd86ae4b9a5c7a9
Xotrab <mokrylisc@gmail.com>
2021-12-04 16:02:21 +0100

    Added the RegisterDto class

---
+++ StreamingApp.Domain/DTOs/RegisterDto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StreamingApp.Domain.DTOs
+{
+    public class RegisterDto
+    {
+        [Required]
+        [MaxLength(20)]
+        public string UserName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [MinLength(5)]
+        [MaxLength(25)]
+        public string Password { get; set; }
+
+        [Required]
+        [MaxLength(25)]
+        public string ConfirmPassword { get; set; }
+    }
+}
