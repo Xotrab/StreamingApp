9dcce549c97aa763297876a7b2f4943ae870b96c
Xotrab <mokrylisc@gmail.com>
2021-12-04 20:20:44 +0100

    Added the mail service implementation to the DI container

--- StreamingApp/Startup.cs
+++ StreamingApp/Startup.cs
@@ -64,6 +64,7 @@ namespace StreamingApp
 
             services.AddScoped<IUserService, UserService>();
             services.AddScoped<ApplicationUserMapper>();
+            services.AddTransient<IMailService, MailService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
