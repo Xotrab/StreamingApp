d3ca4bc55358e481815b64b0441578f962986219
Xotrab <mokrylisc@gmail.com>
2021-12-04 16:33:54 +0100

    Created the UserController with a RegisterAsync method

---
+++ StreamingApp/Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StreamingApp.Domain.DTOs;
+using StreamingApp.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StreamingApp.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService mUserService;
+
+        public UserController(IUserService userService)
+        {
+            mUserService = userService;
+        }
+
+        [HttpPost("/register")]
+        public async Task<IActionResult> RegisterAsync([FromBody]RegisterDto registerDto)
+        { 
+            if (!ModelState.IsValid)
+                return BadRequest("Invalid register data");
+
+            var result = await mUserService.RegisterUserAsync(registerDto);
+
+            if (!result.Success)
+            {
+                return BadRequest(result);
+            }
+            
+            return Ok(result);
+        }
+
+    }
+}
