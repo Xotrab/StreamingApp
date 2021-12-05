23d8fcaa418f5bc15069c0f6b868d930942ffc52
Xotrab <mokrylisc@gmail.com>
2021-12-05 17:18:28 +0100

    Moved the app module routing to the separate App-routing-module file to keep it more transparent

--- StreamingApp/ClientApp/src/app/app.module.ts
+++ StreamingApp/ClientApp/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing-module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 
@@ -15,9 +15,7 @@ import { HomeComponent } from './home/home.component';
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
-    RouterModule.forRoot([
-    { path: '', component: HomeComponent, pathMatch: 'full' }
-], { relativeLinkResolution: 'legacy' })
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
