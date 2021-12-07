ed41a70801c44e9106c7b77bf0f881924fd2fd5a
Xotrab <mokrylisc@gmail.com>
2021-12-07 17:11:05 +0100

    Added theMatDividerModule and the MatIconModule

--- StreamingApp/ClientApp/src/app/shared/shared.module.ts
+++ StreamingApp/ClientApp/src/app/shared/shared.module.ts
@@ -2,17 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatIconModule} from '@angular/material/icon'
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     MatInputModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatDividerModule,
+    MatIconModule
   ],
   exports: [
     MatInputModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatDividerModule,
+    MatIconModule
   ]
 })
 export class SharedModule { }
