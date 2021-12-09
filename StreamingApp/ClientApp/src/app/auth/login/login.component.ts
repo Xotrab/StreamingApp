14b78a25a55af59bb92b105d936fd0aa4632e712
Xotrab <mokrylisc@gmail.com>
2021-12-09 17:35:40 +0100

    Added the submit form method and the method for navigating to the register page, as well as the spinner handling

--- StreamingApp/ClientApp/src/app/auth/login/login.component.ts
+++ StreamingApp/ClientApp/src/app/auth/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LoginDto } from 'src/app/api/dtos/login-dto';
+import { UserService } from 'src/app/api/services/user.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public loginFormGroup: FormGroup = new FormGroup({
+    email: new FormControl('', [Validators.minLength(1), Validators.maxLength(100)]),
+    password: new FormControl('', [Validators.minLength(5), Validators.maxLength(25)])
+  });
 
-  ngOnInit(): void {
+  public showSpinner: boolean = false;
+
+  constructor(private userService: UserService, private router: Router) { }
+
+  public ngOnInit(): void {
+
   }
 
+  public navigateToRegisterForm(): void {
+    this.router.navigate(['/auth/register']);
+  }
+
+  public submitForm(): void {
+    if (this.loginFormGroup.invalid)
+      return;
+    
+    const loginDto: LoginDto = {
+      email: this.loginFormGroup.get('email').value,
+      password: this.loginFormGroup.get('password').value,
+    };
+
+    this.showSpinner = true;
+
+    this.userService.login(loginDto).subscribe(response => {
+      if (response.success) {
+        this.router.navigate(['/']);
+        console.log(response.data);
+      }
+      this.showSpinner = false;
+      }, _ => this.showSpinner = false);
+  }
 }
