6aca9cb459fa93b7a1849a8402c32a416e9b0080
Xotrab <mokrylisc@gmail.com>
2021-12-09 12:33:54 +0100

    Added the register method

--- StreamingApp/ClientApp/src/app/api/services/user.service.ts
+++ StreamingApp/ClientApp/src/app/api/services/user.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { RegisterDto } from '../dtos/register-dto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  public register(registerDto: RegisterDto): Observable<void> {
+    const url = environment.appUrl + '/register';
+    return this.http.post<void>(url, registerDto);
+  }
 }
