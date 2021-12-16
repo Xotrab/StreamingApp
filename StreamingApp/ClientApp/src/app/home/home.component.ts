15c7e42e14811564d43a74d5528552d688372a69
Xotrab <mokrylisc@gmail.com>
2021-12-16 17:21:21 +0100

    The sidebar now emits the selected option value, and parent catches that and changes the variable value

--- StreamingApp/ClientApp/src/app/home/home.component.ts
+++ StreamingApp/ClientApp/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { SidebarOption } from '../helpers/sidebar-option.enum';
 import { JwtTokenService } from '../services/jwt-token.service';
 
 @Component({
@@ -12,6 +13,9 @@ export class HomeComponent implements OnInit {
 
   public isLoggedIn$: Observable<boolean>;
   public userName: string;
+
+  public sidebarOption = SidebarOption;
+  public selectedOption: SidebarOption = this.sidebarOption.Home;
 
   constructor(private router: Router, private jwtTokenService: JwtTokenService) { }
 
@@ -30,5 +34,9 @@ export class HomeComponent implements OnInit {
 
   public logout(): void {
     this.jwtTokenService.logout();
+  }
+
+  public sidebarOptionChanged($event): void {
+    this.selectedOption = $event;
   }
 }
