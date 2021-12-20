321a0e707b0817cf8a56865f30040f8143cf2138
Xotrab <mokrylisc@gmail.com>
2021-12-20 14:28:29 +0100

    Added the migration for the song table genre column type change

---
+++ StreamingApp.Database/Migrations/20211220132628_EnumGenre.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StreamingApp.Database.Migrations
+{
+    public partial class EnumGenre : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Genre",
+                table: "Songs",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Genre",
+                table: "Songs",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
