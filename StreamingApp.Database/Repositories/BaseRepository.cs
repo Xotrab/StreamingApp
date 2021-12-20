4c5dae4f92aab1d28dff3c846658b57eb18f2d59
Xotrab <mokrylisc@gmail.com>
2021-12-20 15:12:01 +0100

    Created the base repository abstract class

---
+++ StreamingApp.Database/Repositories/BaseRepository.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreamingApp.Database.Repositories
+{
+    public abstract class BaseRepository<T> where T : class
+    {
+        protected StreamingAppDbContext mDbContext;
+        protected abstract DbSet<T> mDbSet { get; }
+
+        public BaseRepository(StreamingAppDbContext dbContext)
+        {
+            mDbContext = dbContext;
+        }
+
+        public void Save()
+        {
+            try
+            {
+                mDbContext.SaveChanges();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<T> Get(int id)
+        {
+            var result = await mDbSet.FindAsync(id);
+            return result;
+        }
+    }
+}
