493ff32956734677dfe96e6bd9d1efb0881d3636
Xotrab <mokrylisc@gmail.com>
2021-12-20 19:22:01 +0100

    Changed the folder name from Mappers to MapperProfiles and added the SongProfile

---
+++ StreamingApp.Services/MapperProfiles/SongProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using StreamingApp.Domain.DTOs;
+using StreamingApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreamingApp.Services.Mappers
+{
+    public class SongProfile : Profile
+    {
+        public SongProfile()
+        {
+            CreateMap<UploadSongDto, SongModel>()
+                .ForMember(model => model.Name, x => x.MapFrom(dto => dto.File.FileName))
+                .ForMember(model => model.Url, x => x.MapFrom((src, dst, dstMember, context) => context.Items["Url"]))
+                .ForMember(model => model.AuthorId, x => x.MapFrom((src, dst, dstMember, context) => context.Items["AuthorId"]))
+                .ForMember(model => model.AddedOn, x => x.MapFrom(_ => DateTime.Now));
+        }
+    }
+}
