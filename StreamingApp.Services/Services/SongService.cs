ef83dedc902433ccde27b3fd8dfdbbc4d9d889aa
Xotrab <mokrylisc@gmail.com>
2021-12-20 14:47:22 +0100

    Created the sogn service implementation

---
+++ StreamingApp.Services/Services/SongService.cs
@@ -0,0 +1,19 @@
+﻿using StreamingApp.Domain.DTOs;
+using StreamingApp.Domain.Interfaces;
+using StreamingApp.Shared.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreamingApp.Services
+{
+    public class SongService : ISongService
+    {
+        public Task<Response> AddAsync(UploadSongDto uploadSongDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
