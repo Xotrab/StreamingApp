471787b39ad6eb51967167c5d2b3dbc42194a93a
Xotrab <mokrylisc@gmail.com>
2021-12-20 13:56:07 +0100

    Created the Upload song dto

---
+++ StreamingApp.Domain/DTOs/UploadSongDto.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreamingApp.Domain.DTOs
+{
+    public class UploadSongDto
+    {
+        public IFormFile File { get; set; }
+
+        public string Genre { get; set; }
+    }
+}
