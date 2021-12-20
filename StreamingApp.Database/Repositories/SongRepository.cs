5d61a92eaceb2f6803875ae4dbd8f293d0019a02
Xotrab <mokrylisc@gmail.com>
2021-12-20 15:12:12 +0100

    Created the song repository class

---
+++ StreamingApp.Database/Repositories/SongRepository.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StreamingApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreamingApp.Database.Repositories
+{
+    public class SongRepository : BaseRepository<SongModel>
+    {
+        protected override DbSet<SongModel> mDbSet => mDbContext.Songs;
+
+        public SongRepository(StreamingAppDbContext context) : base(context)
+        {
+                
+        }
+
+        public int Create(SongModel model)
+        {
+            var result = mDbSet.Add(model);
+            Save();
+            return result.Entity.Id;
+        }
+    }
+}
