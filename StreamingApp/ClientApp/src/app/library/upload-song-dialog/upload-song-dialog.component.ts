d4c00db4f9a5956d17bc8dddf761e8d6eb47acc0
Xotrab <mokrylisc@gmail.com>
2021-12-21 18:02:35 +0100

    Added the submit method and the showSpinner property which controlls the spinner visibility

--- StreamingApp/ClientApp/src/app/library/upload-song-dialog/upload-song-dialog.component.ts
+++ StreamingApp/ClientApp/src/app/library/upload-song-dialog/upload-song-dialog.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { UploadSongDto } from 'src/app/api/dtos/upload-song-dto';
+import { SongService } from 'src/app/api/services/song.service';
 import { DialogAction } from 'src/app/helpers/dialog-action.enum';
 import { Genre } from 'src/app/helpers/genre.enum';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-upload-song-dialog',
@@ -10,12 +14,50 @@ import { Genre } from 'src/app/helpers/genre.enum';
 })
 export class UploadSongDialogComponent implements OnInit {
 
-  constructor(public dialogRef: MatDialogRef<UploadSongDialogComponent>) { }
+  constructor(
+    public dialogRef: MatDialogRef<UploadSongDialogComponent>,
+    private snackBar: MatSnackBar,
+    private songService: SongService)
+    { }
 
   public song: File;
   public genreEnum = Genre;
+  public genre: Genre;
+
+  public showSpinner: boolean = false;
 
   public ngOnInit(): void {
+  }
+
+  public submit(): void {
+    if (!this.song || this.genre === undefined) {
+      this.snackBar.open(
+        "Please drop the audio file and select the genre first!", 'Ok', {
+          duration: environment.snackbarDuration
+        }
+      );
+      
+      return;
+    }
+    
+    const uploadSongDto: UploadSongDto = {
+      file: this.song,
+      genre: this.genre
+    };
+
+    this.showSpinner = true;
+
+    this.songService.uploadSong(uploadSongDto).subscribe(result => {
+      this.showSpinner = false;
+
+      this.snackBar.open(
+        "Uploaded successfully", 'Ok', {
+          duration: environment.snackbarDuration
+        }
+      );
+      
+      this.dialogRef.close({ event: DialogAction.Submit, data: result.data });
+    }, _ => this.showSpinner = false);
   }
 
   public close(): void {
