a97ee6f8cb514d5deb11927256e4f6c7e08f82fc
Xotrab <mokrylisc@gmail.com>
2021-12-21 16:20:21 +0100

    Injected the dialogRef and added the methods and properties for the file uploading

--- StreamingApp/ClientApp/src/app/library/upload-song-dialog/upload-song-dialog.component.ts
+++ StreamingApp/ClientApp/src/app/library/upload-song-dialog/upload-song-dialog.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { DialogAction } from 'src/app/helpers/dialog-action.enum';
 
 @Component({
   selector: 'app-upload-song-dialog',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UploadSongDialogComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialogRef: MatDialogRef<UploadSongDialogComponent>) { }
 
-  ngOnInit(): void {
+  public song: File;
+
+  public ngOnInit(): void {
   }
 
+  public close(): void {
+    this.dialogRef.close({ event: DialogAction.Cancel, data: null });
+  }
+
+  public onFileDrop(event): void {
+    this.song = event.addedFiles[0];
+  }
+
+  public onFileRemove(): void {
+    this.song = null;
+  }
 }
