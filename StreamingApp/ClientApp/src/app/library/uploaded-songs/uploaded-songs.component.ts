c50071379d23e9cc8993040f354082c9c28c4f2f
Xotrab <mokrylisc@gmail.com>
2021-12-21 19:53:18 +0100

    Added the hardcoded table data

--- StreamingApp/ClientApp/src/app/library/uploaded-songs/uploaded-songs.component.ts
+++ StreamingApp/ClientApp/src/app/library/uploaded-songs/uploaded-songs.component.ts
@@ -1,6 +1,7 @@
 import { NoopScrollStrategy } from '@angular/cdk/overlay';
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { PlaylistDto } from 'src/app/api/dtos/playlist-dto';
 import { UploadSongDialogComponent } from '../upload-song-dialog/upload-song-dialog.component';
 
 @Component({
@@ -9,6 +10,62 @@ import { UploadSongDialogComponent } from '../upload-song-dialog/upload-song-dia
   styleUrls: ['./uploaded-songs.component.scss']
 })
 export class UploadedSongsComponent implements OnInit {
+
+  public playlist: PlaylistDto = {
+    id: 1,
+    name: "sampleName",
+    songs: [
+      {
+        id: 1,
+        name: "Wielka piwinica",
+        genre: 0,
+        url: "",
+        playbacks: 0,
+        likes: 0,
+        addedOn: new Date(Date.now()),
+        author: {
+          id: 11,
+          email: "b.kaluza99@gmail.com",
+          userName: "Xotrab"
+        }
+      },
+      {
+        id: 2,
+        name: "drill 2",
+        genre: 1,
+        url: "",
+        playbacks: 2137,
+        likes: 34,
+        addedOn: new Date(Date.now()),
+        author: {
+          id: 11,
+          email: "b.kaluza99@gmail.com",
+          userName: "Xotrab"
+        }
+      },
+      {
+        id: 3,
+        name: "love sosa",
+        genre: 1,
+        url: "",
+        playbacks: 1337,
+        likes: 2500,
+        addedOn: new Date(Date.now()),
+        author: {
+          id: 11,
+          email: "b.kaluza99@gmail.com",
+          userName: "Keef"
+        }
+      },
+    ],
+    playbacks: 0,
+    likes: 0,
+    author: {
+      id: 11,
+      email: "b.kaluza99@gmail.com",
+      userName: "Xotrab"
+    }
+  };
 
   constructor(private dialog: MatDialog) { }
 
