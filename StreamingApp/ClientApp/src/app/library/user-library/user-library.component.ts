0145cadb89436e40e04a17fcb06851027e12ff32
Xotrab <mokrylisc@gmail.com>
2021-12-21 18:15:32 +0100

    Changed the default library tab to uploaded for faster development

--- StreamingApp/ClientApp/src/app/library/user-library/user-library.component.ts
+++ StreamingApp/ClientApp/src/app/library/user-library/user-library.component.ts
@@ -9,7 +9,7 @@ import { LibraryTab } from 'src/app/helpers/library-tab.enum';
 export class UserLibraryComponent implements OnInit {
 
   public libraryTab = LibraryTab;
-	public selectedTab: LibraryTab = this.libraryTab.LikedSongs;
+	public selectedTab: LibraryTab = this.libraryTab.Uploaded;
 
   constructor() { }
 
