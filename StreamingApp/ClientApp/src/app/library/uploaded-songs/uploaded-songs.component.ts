793858b1e140bfc9b58528d0a19c17686802e152
Xotrab <mokrylisc@gmail.com>
2021-12-21 13:36:18 +0100

    Generated the uploaded songs component

---
+++ StreamingApp/ClientApp/src/app/library/uploaded-songs/uploaded-songs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-uploaded-songs',
+  templateUrl: './uploaded-songs.component.html',
+  styleUrls: ['./uploaded-songs.component.scss']
+})
+export class UploadedSongsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
