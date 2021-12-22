61eb2f80412735a07efbf5bc14d54f918b2df0c7
Xotrab <mokrylisc@gmail.com>
2021-12-22 21:35:09 +0100

    Added the getUploaded method

--- StreamingApp/ClientApp/src/app/api/services/song.service.ts
+++ StreamingApp/ClientApp/src/app/api/services/song.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { ApiResponse } from '../dtos/api-response';
+import { SongDto } from '../dtos/song-dto';
 import { UploadSongDto } from '../dtos/upload-song-dto';
 
 @Injectable({
@@ -20,5 +21,11 @@ export class SongService {
     formData.append("Genre", uploadSongDto.genre.toString());
 
     return this.http.post<ApiResponse<number>>(url, formData);
+  }
+
+  public getUploaded(): Observable<ApiResponse<SongDto[]>> {
+    const url = environment.appUrl + '/uploads';
+
+    return this.http.get<ApiResponse<SongDto[]>>(url);
   }
 }
