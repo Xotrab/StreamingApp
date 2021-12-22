d60d7f8c85285654af80c96a2604b544f235b77d
Xotrab <mokrylisc@gmail.com>
2021-12-22 15:04:54 +0100

    Added more hardcoded data

--- StreamingApp/ClientApp/src/app/library/uploaded-songs/uploaded-songs.component.ts
+++ StreamingApp/ClientApp/src/app/library/uploaded-songs/uploaded-songs.component.ts
@@ -85,6 +85,76 @@ export class UploadedSongsComponent implements OnInit {
           userName: "Frank Ocean"
         }
       },
+      {
+        id: 1,
+        name: "Wielka piwinica",
+        genre: 0,
+        url: "",
+        playbacks: 0,
+        likes: 0,
+        addedOn: new Date(Date.now()),
+        author: {
+          id: 11,
+          email: "b.kaluza99@gmail.com",
+          userName: "Xotrab"
+        }
+      },
+      {
+        id: 2,
+        name: "drill 2",
+        genre: 1,
+        url: "",
+        playbacks: 2137,
+        likes: 34,
+        addedOn: new Date(Date.now()),
+        author: {
+          id: 11,
+          email: "b.kaluza99@gmail.com",
+          userName: "Xotrab"
+        }
+      },
+      {
+        id: 3,
+        name: "love sosa",
+        genre: 1,
+        url: "",
+        playbacks: 1337,
+        likes: 2500,
+        addedOn: new Date(Date.now()),
+        author: {
+          id: 11,
+          email: "b.kaluza99@gmail.com",
+          userName: "Keef"
+        }
+      },
+      {
+        id: 4,
+        name: "gym class",
+        genre: 8,
+        url: "",
+        playbacks: 1337,
+        likes: 2500,
+        addedOn: new Date(Date.now()),
+        author: {
+          id: 11,
+          email: "b.kaluza99@gmail.com",
+          userName: "Peep"
+        }
+      },
+      {
+        id: 5,
+        name: "Nights",
+        genre: 1,
+        url: "",
+        playbacks: 1337,
+        likes: 2500,
+        addedOn: new Date(Date.now()),
+        author: {
+          id: 11,
+          email: "b.kaluza99@gmail.com",
+          userName: "Frank Ocean"
+        }
+      },
     ],
     playbacks: 0,
     likes: 0,
