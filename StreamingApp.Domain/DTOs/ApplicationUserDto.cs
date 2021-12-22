82009fa98eec5de94abdd8a68d8cafc58ac4c9a9
Xotrab <mokrylisc@gmail.com>
2021-12-22 19:46:21 +0100

    Added the ApplicationUserDto and the SongDto classes

---
+++ StreamingApp.Domain/DTOs/ApplicationUserDto.cs
@@ -0,0 +1,11 @@
+﻿namespace StreamingApp.Domain.DTOs
+{
+    public class ApplicationUserDto
+    {
+        public int Id { get; set; }
+
+        public string Email { get; set; }
+
+        public string UserName { get; set; }
+    }
+}
