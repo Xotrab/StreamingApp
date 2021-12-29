d7ae52af885df49f40b25401415fbc77fbe0d174
Xotrab <mokrylisc@gmail.com>
2021-12-29 21:57:07 +0100

    Added the mat menu for the playlist brief card - the menu is only shown if the user is the playlist owner

--- StreamingApp/ClientApp/src/app/shared/playlist-brief-card/playlist-brief-card.component.ts
+++ StreamingApp/ClientApp/src/app/shared/playlist-brief-card/playlist-brief-card.component.ts
@@ -1,6 +1,8 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { MatMenuTrigger } from '@angular/material/menu';
 import { Observable } from 'rxjs';
 import { PlaylistBriefDto } from 'src/app/api/dtos/playlist-brief-dto';
+import { MenuPosition } from 'src/app/helpers/menu-position';
 import { JwtTokenService } from 'src/app/services/jwt-token.service';
 
 @Component({
@@ -11,8 +13,16 @@ import { JwtTokenService } from 'src/app/services/jwt-token.service';
 export class PlaylistBriefCardComponent implements OnInit {
 
   @Input() playlistBrief: PlaylistBriefDto;
+  @Input() showMenu: boolean = false;
 
   public isLoggedIn$: Observable<boolean>;
+
+  @ViewChild(MatMenuTrigger, {static: true}) matMenuTrigger: MatMenuTrigger;
+
+  public menuPosition: MenuPosition = {
+    x:'0',
+    y:'0'
+  };
 
   constructor(private jwtTokenService: JwtTokenService) { }
 
@@ -23,4 +33,18 @@ export class PlaylistBriefCardComponent implements OnInit {
   public tooglePlaylistLike(): void {
     //this.isLiked = !this.isLiked;
   }
+
+  public onRightClick(event: MouseEvent): void {
+    if (!this.showMenu) {
+      return;
+    }
+    event.preventDefault(); 
+
+    this.menuPosition.x = event.clientX + 'px'; 
+    this.menuPosition.y = event.clientY + 'px'; 
+
+    this.matMenuTrigger.menuData = { playlistBrief: this.playlistBrief };
+
+    this.matMenuTrigger.openMenu(); 
+} 
 }
