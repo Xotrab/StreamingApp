18a49eaca1f4bf8b61f40f39b87fce517e6d99bf
Xotrab <mokrylisc@gmail.com>
2021-12-29 15:04:31 +0100

    Added the PlaylistDto

---
+++ StreamingApp.Domain/DTOs/PlaylistDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreamingApp.Domain.DTOs
+{
+    public class PlaylistDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public List<SongDto> Songs { get; set; }
+
+        public int Playbacks { get; set; }
+
+        public int Likes { get; set; }
+
+        public bool LikedByUser { get; set; }
+
+        public ApplicationUserDto Author { get; set; }
+    }
+}
