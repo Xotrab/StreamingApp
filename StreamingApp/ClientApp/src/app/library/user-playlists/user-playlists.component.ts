bd7ac0363e621b88948e0a7759f4bed3fad250c6
Xotrab <mokrylisc@gmail.com>
2021-12-29 20:30:19 +0100

    Added the method for opening the create playlist dialog

--- StreamingApp/ClientApp/src/app/library/user-playlists/user-playlists.component.ts
+++ StreamingApp/ClientApp/src/app/library/user-playlists/user-playlists.component.ts
@@ -1,6 +1,9 @@
+import { NoopScrollStrategy } from '@angular/cdk/overlay';
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { PlaylistBriefDto } from 'src/app/api/dtos/playlist-brief-dto';
 import { PlaylistService } from 'src/app/api/services/playlist.service';
+import { CreatePlaylistDialogComponent } from '../create-playlist-dialog/create-playlist-dialog.component';
 
 @Component({
   selector: 'app-user-playlists',
@@ -11,10 +14,14 @@ export class UserPlaylistsComponent implements OnInit {
 
   public playlistBriefs: Array<PlaylistBriefDto>;
 
-  constructor(private playlistService: PlaylistService) { }
+  constructor(private playlistService: PlaylistService, private dialog: MatDialog) { }
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
     this.playlistService.getUserPlaylistBriefs().subscribe(result => this.playlistBriefs = result.data);
+  }
+
+  public openCreatePlaylistDialog(): void {
+    this.dialog.open(CreatePlaylistDialogComponent, { disableClose: true, scrollStrategy: new NoopScrollStrategy() });
   }
 
 }
